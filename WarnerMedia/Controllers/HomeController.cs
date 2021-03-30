93efe624c82ee41d0e3ced58c6dfa67f62a857ec
jlebo <jlebo@DESKTOP-NQCDN4B>
2021-03-29 20:44:43 -0400

    save this

---
+++ WarnerMedia/Controllers/HomeController.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using WarnerMedia.Models;
+using System.Net.Http.Headers;
+
+namespace WarnerMedia.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        public static HttpClient webClient = new HttpClient();
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+            webClient.BaseAddress = new Uri("https://localhost:44365/weatherforecast");
+            webClient.DefaultRequestHeaders.Clear();
+            webClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+        }
+
+        public IActionResult Index()
+        {
+            var result = webClient.GetAsync("Get").Result;
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        public IActionResult Search()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
